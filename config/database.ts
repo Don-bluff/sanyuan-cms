141fe54cade2e5ceef282257673e180ae5042a6c
Donbluff <gamifiedlife@donbluff.com>
2025-05-30 01:37:30 -0400

    fix database code

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,25 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: env('DATABASE_URL'),
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-});
+module.exports = ({ env }) => {
+  const dbUrl = env('DATABASE_URL');
+
+  return {
+    connection: dbUrl
+      ? {
+          client: 'postgres',
+          connection: dbUrl,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
+      : {
+          client: 'postgres',
+          connection: {
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'sanyuan-cms'),
+            user: env('DATABASE_USERNAME', 'postgres'),
+            password: env('DATABASE_PASSWORD', 'your-password'),
+            ssl: false,
+          },
+        },
+  };
+};
