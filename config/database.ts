97b93b8ac73817211a7fa8b7816115e29208e3e2
Donbluff <gamifiedlife@donbluff.com>
2025-05-30 01:27:40 -0400

    fix database

--- config/database.ts
+++ config/database.ts
@@ -1,20 +1,9 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'sanyuan-cms'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '9664898767'),
-      ssl: env.bool('DATABASE_SSL', false) ? {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-      } : false,
+    connection: env('DATABASE_URL'),
+    ssl: {
+      rejectUnauthorized: false,
     },
-    pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
-    },
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
   },
 });
